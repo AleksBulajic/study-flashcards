013da9b6b6b144c5b1cacf8a43418680c9cb098d
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-08-16 14:04:43 -0400

    navbar footer components added to the app

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavBar.css"
+
+function NavBar() {
+  return (
+    <div className="nav-container">
+      <h1 className="title-nav">StudyHub</h1>
+    </div>
+  );
+}
+
+export default NavBar;
