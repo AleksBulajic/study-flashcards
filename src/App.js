46ad2852f5aa5a18484dd6ff49e353c30eda44cb
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-08-16 16:47:15 -0400

    need to figure out how not to change css when adding link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import About from "./pages/about/About";
+import Source from "./pages/source/Source";
 
 function App() {
   return (
     <div className="App">
-<NavBar />
+      <NavBar />
+      <Routes>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/source" component={Source} />
+      </Routes>
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./NavBar.css";
+import { Link } from "react-router-dom";
 import Cover from "../../assets/cover.png";
 
 function NavBar() {
