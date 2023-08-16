c5573553abe1c91e50b26c73279585e7145ba5ba
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-08-16 15:05:03 -0400

    pages folder added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import "./NavBar.css"
-import Cover from "../../assets/cover.png"
+import "./NavBar.css";
+import Cover from "../../assets/cover.png";
 
 function NavBar() {
   return (
     <div className="nav-container">
-      <img src={Cover} className="navbar-png" alt="cover-img"/>
+      <img src={Cover} className="navbar-png" alt="cover-img" />
+      <div className="nav-text">
+        <p className="source">Source</p>
+        <p className="about">About</p>
+      </div>
     </div>
   );
 }
