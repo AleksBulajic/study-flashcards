192a3afb31063209cafca9ce250981542349213c
AleksBulajic <aleks.bulajic25@gmail.com>
2023-10-09 16:50:05 -0400

    managed to send data from back to front end

--- src/components/Flashcard/FlashCard.jsx
+++ src/components/Flashcard/FlashCard.jsx
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './FlashCard.css';
 
 function FlashCard({ question, answer }) {
   const [isFlipped, setIsFlipped] = useState(false);
+  const [isQuestion, setIsQuestion] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/questions/all');
+        console.log(response.data); 
+      } catch (error) {
+        console.error('Error getting data:', error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
+  
 
   const flipCard = () => {
     setIsFlipped(!isFlipped);
