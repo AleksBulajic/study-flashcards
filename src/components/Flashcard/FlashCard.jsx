f37d46d6f2a15d5a8209f69c70e49625f9b42a0e
AleksBulajic <aleks.bulajic25@gmail.com>
2023-10-10 11:27:15 -0400

    swithced functionality of flash card in the component

--- src/components/Flashcard/FlashCard.jsx
+++ src/components/Flashcard/FlashCard.jsx
@@ -1,32 +1,63 @@
 import React, { useState, useEffect } from "react";
-
 import "./FlashCard.css";
+import axios from "axios";
 
-function FlashCard({ question, answer }) {
+function FlashCard() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
   const [isFlipped, setIsFlipped] = useState(false);
-
-
-
+  const [allQuestions, setAllQuestions] = useState([]);
+  
   const flipCard = () => {
     setIsFlipped(!isFlipped);
   };
 
-  const content = isFlipped ? answer : question;
+  const handleNext = () => {
+    setIsFlipped(false);
+    setCurrentQuestion((currentQuestion + 1) % allQuestions.length);
+  };
+  
+  const handlePrevious = () => {
+    setIsFlipped(false);
+    setCurrentQuestion(
+      (currentQuestion - 1 + allQuestions.length) % allQuestions.length
+    );
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/questions/all");
+        console.log(response.data);
+        setAllQuestions(response.data);
+      } catch (error) {
+        console.error("Error getting data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const currentQuestionData = allQuestions[currentQuestion] || {};
+
+  const content = isFlipped ? currentQuestionData.answer : currentQuestionData.question;
 
   return (
-    <div
-      className={`flashcard-container ${isFlipped ? "flipped" : ""}`}
-      onClick={flipCard}
-    >
-      <div className="flashcard">
-        <div className={`card ${isFlipped ? "flipped" : ""}`}>
-          <div className="card-content">
-            <h2>{isFlipped ? "Answer" : "Question"}</h2>
-            <p>{content}</p>
+    <>
+      <div className="flashcard-container" onClick={flipCard}>
+        <div className="flashcard">
+          <div className={`card ${isFlipped ? "flipped" : ""}`}>
+            <div className="card-content">
+              <h2>{isFlipped ? "Answer" : "Question"}</h2>
+              <p>{content}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="navigation-buttons">
+        <button onClick={handlePrevious}>Previous</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
+    </>
   );
 }
 
