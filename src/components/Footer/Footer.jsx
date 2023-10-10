b4f3a65f222403819b1af74925647b1d4bf7796d
AleksBulajic <aleks.bulajic25@gmail.com>
2023-10-10 15:13:58 -0400

    css fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Footer.css';
+import Cover from "../../assets/cover.png"
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
         <p>&copy; 2023 StudyHub. All rights reserved.</p>
+        
       </div>
+      {/* <img src={Cover} className="footer-png" alt="cover-img" /> */}
     </footer>
   );
 }
