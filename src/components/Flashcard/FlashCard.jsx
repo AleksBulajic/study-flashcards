75108cec2fdbe9bd6ded29528ab65e05c4d9fd42
AleksBulajic <aleks.bulajic25@gmail.com>
2023-10-10 10:32:52 -0400

    able to set question in flash card, added buttons for next question and previus question

--- src/components/Flashcard/FlashCard.jsx
+++ src/components/Flashcard/FlashCard.jsx
@@ -1,47 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './FlashCard.css';
+import React, { useState, useEffect } from "react";
+
+import "./FlashCard.css";
 
 function FlashCard({ question, answer }) {
   const [isFlipped, setIsFlipped] = useState(false);
-  const [isQuestion, setIsQuestion] = useState('')
-  const [allQuestions, setAllQuestions] = useState([])
 
-  useEffect(() => {
-    console.log('allQuestions:', allQuestions)
-  }, [allQuestions])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:3000/questions/all');
-        console.log(response.data); 
-      setAllQuestions(response.data);
-      
-      } catch (error) {
-        console.error('Error getting data:', error);
-      }
-    };
-  
-    fetchData();
-  }, []);
-  
-  // TODO create a state qurent question,  and button for the next question 
-  
 
   const flipCard = () => {
     setIsFlipped(!isFlipped);
   };
 
-
   const content = isFlipped ? answer : question;
 
   return (
-    <div className={`flashcard-container ${isFlipped ? 'flipped' : ''}`} onClick={flipCard}>
+    <div
+      className={`flashcard-container ${isFlipped ? "flipped" : ""}`}
+      onClick={flipCard}
+    >
       <div className="flashcard">
-        <div className={`card ${isFlipped ? 'flipped' : ''}`}>
+        <div className={`card ${isFlipped ? "flipped" : ""}`}>
           <div className="card-content">
-            <h2>{isFlipped ? 'Answer' : 'Question'}</h2>
+            <h2>{isFlipped ? "Answer" : "Question"}</h2>
             <p>{content}</p>
           </div>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
+import axios from "axios";
 import FlashCard from "../../components/Flashcard/FlashCard";
 
 function Home() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [isQuestion, setIsQuestion] = useState("");
+  const [allQuestions, setAllQuestions] = useState([]);
+
+  useEffect(() => {
+    console.log("allQuestions:", allQuestions);
+  }, [allQuestions]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/questions/all");
+        console.log(response.data);
+        setAllQuestions(response.data);
+      } catch (error) {
+        console.error("Error getting data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const handleNext = () => {
+    // Increase the question index, and handle wrapping around.
+    setCurrentQuestion((currentQuestion + 1) % allQuestions.length);
+  };
+
+  const handlePrevious = () => {
+    // Decrement the question index, and handle wrapping around.
+    setCurrentQuestion(
+      (currentQuestion - 1 + allQuestions.length) % allQuestions.length
+    );
+  };
+
   return (
     <>
-        <div className="home-container">
-      <FlashCard
-        question="What is React?"
-        answer="React is a JavaScript library for building user interfaces."
-      />
-    </div>
+      <div className="home-container">
+        <FlashCard
+          question={allQuestions[currentQuestion]?.question}
+          answer={allQuestions[currentQuestion]?.answer}
+        />
+        <div className="navigation-buttons">
+          <button onClick={handlePrevious}>Previous</button>
+          <button onClick={handleNext}>Next</button>
+        </div>
+      </div>
     </>
   );
 }
